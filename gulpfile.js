8ef7dfcb22086b02c5440bbdbe7394b4785a7964
Fernan de Dios <info@mediagramstudios.com>
2017-10-19 15:51:32 +0800

    tasks for js cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload; // enable reload
 
 var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
 
 // files on src folder
 var SOURCEPATHS = {
   sassSource: 'src/scss/*.scss', // all scss files
-  htmlSource: 'src/*.html' // all html files
+  htmlSource: 'src/*.html', // all html files
+  jsSource: 'src/js/**' // all in js folder
 };
 
 // files in the app folder
@@ -19,7 +21,23 @@ var APPPATH = {
   js: 'app/js'
 };
 
-// SASS task
+// garbage collection task for html files
+gulp.task('clean-html', function() {
+  // check APPPATH.root for unused html files (no src counterpart)
+  // no need to read file contents, just reference filename
+  return gulp.src(APPPATH.root + '/*.html', { read: false, force: true })
+    .pipe(clean());
+});
+
+// garbage collection task for js files
+gulp.task('clean-scripts', function() {
+  // check APPPATH.root for unused js files (no src counterpart)
+  // no need to read file contents, just reference filename
+  return gulp.src(APPPATH.js + '/*.js', { read: false, force: true })
+    .pipe(clean());
+});
+
+// SASS task, take special care of the pipe order
 gulp.task('sass', function() {
   return gulp.src(SOURCEPATHS.sassSource) // define sass files
     .pipe(autoprefixer()) // enable autoprefixer
@@ -27,8 +45,16 @@ gulp.task('sass', function() {
     .pipe(gulp.dest(APPPATH.css)); // define where to output css file
 });
 
+// javascript copy task
+// do clean-scripts task as well for unused js files
+gulp.task('scripts', ['clean-scripts'], function() {
+  gulp.src(SOURCEPATHS.jsSource) // defile javascript src
+    .pipe(gulp.dest(APPPATH.js)); // define where to copy js files
+});
+
 // task to copy html files from src to app folder
-gulp.task('copy', function() {
+// do clean-html task as well for unused html files
+gulp.task('copy', ['clean-html'], function() {
   gulp.src(SOURCEPATHS.htmlSource)
     .pipe(gulp.dest(APPPATH.root))
 });
@@ -40,17 +66,15 @@ gulp.task('serve', ['sass'], function() { // start tasks in the array first befo
   browserSync.init(
     // init using all css, html, js files
     [ APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js' ],
-    {
-      server: {
-        baseDir: APPPATH.root // start server on this folder
-      }
-    });
+    { server: { baseDir: APPPATH.root } } // start server on this folder
+  );
 });
 
 // unified watch task
-gulp.task('watch', ['serve', 'sass', 'copy'], function() { // add tasks in an array
+gulp.task('watch', ['serve', 'sass', 'scripts', 'copy', 'clean-html', 'clean-scripts'], function() { // add tasks in an array
   gulp.watch([SOURCEPATHS.sassSource], ['sass']); // watch scss files for changes. run sass task if detected
   gulp.watch([SOURCEPATHS.htmlSource], ['copy']); // watch html files for changes. run copy task if detected
+  gulp.watch([SOURCEPATHS.jsSource], ['scripts']); // watch js files for changes. run scripts task if detected
 });
 
 gulp.task('default', ['watch']); // launch watch task as default
