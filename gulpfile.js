8116b71b46452b42329fc54ba69e6132e34eb0b6
Fernan de Dios <info@mediagramstudios.com>
2017-10-19 13:01:10 +0800

    Copy task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,42 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+
 var browserSync = require('browser-sync');
 var reload = browserSync.reload; // enable reload
 
+var autoprefixer = require('gulp-autoprefixer');
+
 // files on src folder
 var SOURCEPATHS = {
-  sassSource: 'src/scss/*.scss' // all scss files
-}
+  sassSource: 'src/scss/*.scss', // all scss files
+  htmlSource: 'src/*.html' // all html files
+};
 
 // files in the app folder
 var APPPATH = {
   root: 'app/',
   css: 'app/css',
   js: 'app/js'
-}
+};
 
 // SASS task
 gulp.task('sass', function() {
   return gulp.src(SOURCEPATHS.sassSource) // define sass files
+    .pipe(autoprefixer()) // enable autoprefixer
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError)) // define sass output style
     .pipe(gulp.dest(APPPATH.css)); // define where to output css file
 });
 
-// browser-sync server config
+// task to copy html files from src to app folder
+gulp.task('copy', function() {
+  gulp.src(SOURCEPATHS.htmlSource)
+    .pipe(gulp.dest(APPPATH.root))
+});
+
+// browser-sync server task
 gulp.task('serve', ['sass'], function() { // start tasks in the array first before running server
 
+  // browser-sync config
   browserSync.init(
     // init using all css, html, js files
     [ APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js' ],
@@ -36,8 +48,9 @@ gulp.task('serve', ['sass'], function() { // start tasks in the array first befo
 });
 
 // unified watch task
-gulp.task('watch', ['serve', 'sass'], function() { // add tasks in an array
+gulp.task('watch', ['serve', 'sass', 'copy'], function() { // add tasks in an array
   gulp.watch([SOURCEPATHS.sassSource], ['sass']); // watch scss files for changes. run sass task if detected
+  gulp.watch([SOURCEPATHS.htmlSource], ['copy']); // watch html files for changes. run copy task if detected
 });
 
 gulp.task('default', ['watch']); // launch watch task as default
